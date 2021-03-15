5b928236896bfce0042f7000471b3fa9ae37cdc9
stm37 <79359895+stm37@users.noreply.github.com>
2021-03-15 12:53:03 +0800

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  conn.sendContact(m.chat, '62895337278647', 'Owner Kuhong', m)
+  // this.sendContact(m.chat, '62895605030379', 'junaedi', m)
+  conn.sendContact(m.chat, '62895605030379', 'Owner Kuhong', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
